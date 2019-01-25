48cd14343442b8267338be109d4c323aa7070dde
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-25 22:26:03 +0300

    select post

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -4,17 +4,21 @@ import './FullPost.css';
 
 class FullPost extends Component {
     render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
 
-        );
+        if (this.props.id) {
+            post = (
+                <div className="FullPost">
+                    <h1>Title</h1>
+                    <p>Content</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+    
+            );
+        }
+                
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount () {
@@ -29,13 +30,20 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({
+            selectedPostId: id
+        });
+    }
+
     render () {
 
         const posts = this.state.posts.map(post => {
             return <Post 
                         key={post.id} 
                         title={post.title}
-                        author={post.author} />;
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)} />;
         });
 
         return (
@@ -44,7 +52,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
