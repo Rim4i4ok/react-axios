22b36fc1d10f5afb2b745ebb28070a51f4fb031e
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-25 22:17:12 +0300

    getting posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,20 +8,30 @@ import './Blog.css';
 
 class Blog extends Component {
 
+    state = {
+        posts: []
+    }
+
     componentDidMount () {
         axios.get('http://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response);
+                this.setState({
+                    posts: response.data
+                });
+                // console.log(response);
             });
     }
 
     render () {
+
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} />;
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
