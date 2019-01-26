e03afa1da58ea69a60f0f8912342d11b21e39e91
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-26 22:39:51 +0300

    axios global intercept response request

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+axios.interceptors.request.use(request => {
+    console.log(request);
+
+    // Edit request config
+    return request;
+}, error => {
+    // On send request
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+
+    // Edit request config
+    return response;
+}, error => {
+    // On response
+    console.log(error);
+    return Promise.reject(error);
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
