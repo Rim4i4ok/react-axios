7ec5a87f4b0a6e84f1207d3f5848e28457e1010e
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-29 22:44:13 +0300

    Route props

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -10,6 +10,10 @@ class NewPost extends Component {
         author: 'Rim'
     }
 
+    componentDidMount () {
+        console.log(this.props);
+    }
+
     postDataHandler = () => {
         const data = {
             title: this.state.title,
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -12,6 +12,9 @@ class Posts extends Component {
     }
 
     componentDidMount () {
+
+        console.log(this.props);
+
         axios.get('/posts')
             .then(response => {
                 const posts = response.data.slice(0, 5);
