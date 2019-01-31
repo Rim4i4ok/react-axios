b5ad09c397fea622ab9d6ada79aeab3c6a37d4c9
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-31 22:27:14 +0300

    Redirect route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -32,8 +32,10 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}                
                 <Switch>  
-                    <Route path="/new-post" component={NewPost} />                    
-                    <Route path="/posts" component={Posts} />                                                      
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts" />
+                    {/* <Route path="/" component={Posts} /> */}
                 </Switch>                
             </div>
         );
