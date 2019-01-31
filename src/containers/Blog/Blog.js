b0283ef91dff780a4ce2e86ff430f9e51e59bcce
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-31 22:40:14 +0300

    protect route for auth

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,6 +7,11 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+
+    state = {
+        auth: false
+    }
+
     render () {
         return (
             <div className="Blog">
@@ -31,8 +36,8 @@ class Blog extends Component {
                 </header>   
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}                
-                <Switch>  
-                    <Route path="/new-post" component={NewPost} />
+                <Switch> 
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}                    
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                     {/* <Route path="/" component={Posts} /> */}
