9643759a06e2f16ac6e41190dae5b82909b9f291
Rim4i4ok <rim4i4ok@gmail.com>
2019-01-31 22:56:27 +0300

    lasy loading components

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,12 +4,16 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+// import NewPost from './NewPost/NewPost';
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
 
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -37,7 +41,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}                
                 <Switch> 
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}                    
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}                    
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not found</h1>} />
                     {/* <Redirect from="/" to="/posts" /> */}
